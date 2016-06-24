e409efddc4fb354adccafde7412f98e6fe4d8fd6
zellman01 <zellman01@gmail.com>
2016-06-24 14:32:36 -0400

    Fixed some more errors

--- chat-plugins/battletest.js
+++ chat-plugins/battletest.js
@@ -3,21 +3,11 @@
 exports.commands = {
 	bt: 'battletest',
 	battletest: function (target, room, user) {
-		target = this.splitTarget(target, true);
-		let targetUser = this.targetUser;
-		let targetUsername = this.targetUsername;
-	
 		if (!this.can('roomvoice')) return this.errorReply('You do not have access to this command.');
 		if (!this.canTalk()) return this.errorReply('You cannot do this command while you are unable to talk!');
-		//if (!target) return this.parse("/help battletest")
 		
-		if (targetUsername = "undefined") {
-			this.add("The battletest could not be started.")
-			console.log("The battletest could not start in " + room.id + " because of the undefinied error.")
-		} else {
-			this.add(targetUsername + " has started a battle test on " + targetUser + ".");
+			this.add(targetUsername + " has started a battle test.");
 			this.logModCommand(targetUsername + " has started a battletest.");
-		}
 	},
-	battletesthelp: ["/battletest (/bt) [user]: Declares to the chat room that you are starting a battle test on a specific user."]
+	battletesthelp: ["/battletest (/bt) [user]: Declares to the chat room that you are starting a battle test."]
 };

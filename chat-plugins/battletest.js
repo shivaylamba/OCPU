89b530d26484c48503aafb2f40abe20832bc76c6
Rainzye <ectdevgist@mailinator.com>
2016-06-24 22:50:48 +0100

    lst --> let

--- chat-plugins/battletest.js
+++ chat-plugins/battletest.js
@@ -1,9 +1,9 @@
-/*'use strict';
+'use strict';
 
 exports.commands = {
 	bt: 'battletest',
 	battletest: function (target, room, user) {
-		lst targetUsername = this.targetUsername;
+		let targetUsername = this.targetUsername;
 		
 		if (!this.can('roomvoice')) return this.errorReply('You do not have access to this command.');
 		if (!this.canTalk()) return this.errorReply('You cannot do this command while you are unable to talk!');
@@ -12,4 +12,4 @@ exports.commands = {
 			this.logModCommand(targetUsername + " has started a battletest.");
 	},
 	battletesthelp: ["/battletest (/bt) [user]: Declares to the chat room that you are starting a battle test."]
-};*/
+};

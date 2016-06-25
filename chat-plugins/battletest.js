f864d408f21992677ce78a88eda6ead6f5eea76e
zellman01 <zellman01@gmail.com>
2016-06-24 22:57:58 -0400

    Atttempted fix of the "undefined" error

--- chat-plugins/battletest.js
+++ chat-plugins/battletest.js
@@ -3,13 +3,16 @@
 exports.commands = {
 	bt: 'battletest',
 	battletest: function (target, room, user) {
-		let targetUsername = this.targetUsername;
+		target = this.splitTarget(target, true);
+		let targetUser = this.targetUser;
+		let name = this.targetUsername;
+		let userid = toId(name);
 		
 		if (!this.can('roomvoice')) return this.errorReply('You do not have access to this command.');
 		if (!this.canTalk()) return this.errorReply('You cannot do this command while you are unable to talk!');
 		
-			this.add(targetUsername + " has started a battle test.");
-			this.logModCommand(targetUsername + " has started a battletest.");
+		this.add(user.name + " has started a battle test on " + name + ".");
+		this.logModCommand(user.name + " has started a battletest.");
 	},
 	battletesthelp: ["/battletest (/bt) [user]: Declares to the chat room that you are starting a battle test."]
 };

2fa05ee2b580e6cf60004032aab57755aa0fadca
zellman01 <zellman01@gmail.com>
2016-06-27 12:18:20 -0400

    Update battletest.js

--- chat-plugins/battletest.js
+++ chat-plugins/battletest.js
@@ -8,15 +8,15 @@ exports.commands = {
 		let name = this.targetUsername;
 		let userid = toId(name);
 		
-		if (!this.can('roomvoice')) return this.errorReply('You do not have access to this command.');
-		if (!this.canTalk()) return this.errorReply('You cannot do this command while you are unable to talk!');
+		if (!this.can('roomvoice')) false return this.errorReply('You do not have access to this command.');
+		if (!this.canTalk()) false return this.errorReply('You cannot do this command while you are unable to talk!');
 		//if (!target) return this.parse('/help battletest');
 		
 		this.add(user.name + " has started a battle test on " + name + ".");
 		this.logModCommand(user.name + " has started a battletest.");
 		
 		if (targetUser) {
-			targetUser.popup(name + " has started a battle test on you in " + room.id + ". Good luck.");
+			targetUser.popup(user.name + " has started a battle test on you in " + room.id + ". Good luck.");
 		}
 	},
 	battletesthelp: ["/battletest (/bt) [user]: Declares to the chat room that you are starting a battle test for a person."],
@@ -29,8 +29,8 @@ exports.commands = {
 		let userid = toId(name);
 		//if (!target) return this.parse('/help battletestclear');
 		
-		if (!this.can('roomvoice')) return this.errorReply('You do not have access to this command.');
-		if (!this.canTalk()) return this.errorReply("You cannot do this command while you are unable to talk!");
+		if (!this.can('roomvoice')) false return this.errorReply('You do not have access to this command.');
+		if (!this.canTalk()) false return this.errorReply("You cannot do this command while you are unable to talk!");
 		
 		this.add(user.name + " has completed the battle test for " + name + "!");
 		if (targetUser) {

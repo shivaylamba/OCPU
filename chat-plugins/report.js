1922669eb5182ec7d7a7b6e65417b784a4a47fd9
Mystifi <bornfromthemyst@gmail.com>
2016-07-08 17:10:06 -0500

    Enforce strict mode; various updates

--- chat-plugins/report.js
+++ chat-plugins/report.js
@@ -2,10 +2,14 @@
 * Reports Plugin *
 * Credits: jd    *
 *****************/
-var fs = require('fs');
-var moment = require('moment');
 
-var Reports = {};
+'use strict';
+
+const fs = require('fs');
+const moment = require('moment');
+
+let Reports = {};
+
 function loadReports() {
 	try {
 		Reports = JSON.parse(fs.readFileSync('config/reports.json'));
@@ -19,21 +23,16 @@ function saveReports() {
 	fs.writeFile('config/reports.json', JSON.stringify(Reports));
 }
 
-function pmUpperStaff(message) {
-    Gold.pmUpperStaff(message, '~Server');
-}
-
 exports.commands = {
-    complain: 'requesthelp',
-    bitch: 'requesthelp',
+    	complain: 'requesthelp',
+    	bitch: 'requesthelp',
 	report: 'requesthelp',
 	requesthelp: function (target, room, user) {
-		if (user.can('pban')) return this.parse('/reports ' + (target || ''));
+		if (!user.can('pban')) return false; // check if a user cannot use the command rather than if they can
 		if (!this.canTalk()) return this.errorReply("You can't use this command while unable to speak.");
 		if (!target) return this.errorReply("Usage: /requesthelp [message] - Requests help from Senior Staff. Please remember to include as much detail as possible with your request.");
 		if (target.length < 1) return this.errorReply("Usage: /requesthelp [message] - Requests help from Senior Staff. Please remember to include as much detail as possible with your request.");
-
-		var reportId = (Object.size(Reports) + 1);
+		let reportId = (Object.size(Reports) + 1);
 		while (Reports[reportId]) reportId--;
 		Reports[reportId] = new Object();
 		Reports[reportId].reporter = user.name;
@@ -42,9 +41,9 @@ exports.commands = {
 		Reports[reportId].status = 'Pending Staff';
 		Reports[reportId].reportTime = moment().format('MMMM Do YYYY, h:mm A') + " EST";
 		saveReports();
+		// does not need a unique function simply because global.pmUpperStaff = pmUpperStaff
 		pmUpperStaff('A new report has been submitted by ' + user.name + '. ID: ' + reportId + ' Message: ' + target.trim());
-		Rooms('upperstaff').add('A new report has been submitted by ' + user.name + '. ID: ' + reportId + ' Message: ' + target.trim());
-		Rooms('upperstaff').update();
+		Rooms('upperstaff').add('A new report has been submitted by ' + user.name + '. ID: ' + reportId + ' Message: ' + target.trim()).update();
 		return this.sendReply("Your report has been sent to Senior Staff.");
 	},
 
@@ -52,18 +51,17 @@ exports.commands = {
 		if (!user.can('pban')) return this.errorReply('/reports - Access denied.');
 		if (!target) var target = '';
 		target = target.trim();
-
-		var cmdParts = target.split(' ');
-		var cmd = cmdParts.shift().trim().toLowerCase();
-		var params = cmdParts.join(' ').split(',').map(function (param) { return param.trim(); });
+		let cmdParts = target.split(' ');
+		let cmd = cmdParts.shift().trim().toLowerCase();
+		let params = cmdParts.join(' ').split(',').map(function (param) { return param.trim(); });
 		switch (cmd) {
 			case '':
 			case 'view':
 				if (!this.runBroadcast()) return;
-				var output = '|raw|<table border="1" cellspacing ="0" cellpadding="3"><tr><th>ID</th><th>Reporter</th><th>Message</th><th>Report Time</th><th>Status</th></tr>';
-				for (var u in Object.keys(Reports)) {
-					var currentReport = Reports[Object.keys(Reports)[u]];
-					var date = currentReport.reportTime;
+				let output = '|raw|<table border="1" cellspacing ="0" cellpadding="3"><tr><th>ID</th><th>Reporter</th><th>Message</th><th>Report Time</th><th>Status</th></tr>';
+				for (let u in Object.keys(Reports)) {
+					let currentReport = Reports[Object.keys(Reports)[u]];
+					let date = currentReport.reportTime;
 					output += '<tr><td>' + currentReport.id + '</td><td>' + Tools.escapeHTML(currentReport.reporter) + '</td><td>' +
 						Tools.escapeHTML(currentReport.message) + '</td><td>' + date + ' </td><td>' + (currentReport.status === 'Pending Staff' ? '<font color=blue>Pending Staff</font>' : (~currentReport.status.indexOf('Accepted by') ? '<font color=green>' + Tools.escapeHTML(currentReport.status) + '</font>' : Tools.escapeHTML(currentReport.status))) + '</td></tr>';
 				}
@@ -71,7 +69,7 @@ exports.commands = {
 				break;
 			case 'accept':
 				if (params.length < 1) return this.errorReply("Usage: /reports accept [id]");
-				var id = params.shift();
+				let id = params.shift();
 				if (!Reports[id]) return this.errorReply("There's no report with that id.");
 				if (Reports[id].status !== 'Pending Staff') return this.errorReply("That report isn't pending staff.");
 				Reports[id].status = "Accepted by " + user.name;
@@ -81,13 +79,12 @@ exports.commands = {
 				}
 				this.sendReply("You've accepted the report by "+ Reports[id].reporter);
 				pmUpperStaff(user.name + " accepted the report by " + Reports[id].reporter + ". (ID: " + id + ")");
-				Rooms('upperstaff').add(user.name + " accepted the report by " + Reports[id].reporter + ". (ID: " + id + ")");
-				Rooms('upperstaff').update();
+				Rooms('upperstaff').add(user.name + " accepted the report by " + Reports[id].reporter + ". (ID: " + id + ")").update();
 				break;
 			case 'decline':
 			case 'deny':
 				if (params.length < 1) return this.errorReply("Usage: /reports deny [id]");
-				var id = params.shift();
+				let id = params.shift();
 				if (!Reports[id]) return this.errorReply("There's no report with that id.");
 				if (Reports[id].status !== 'Pending Staff') return this.errorReply("That report isn't pending staff.");
 				if (Users(Reports[id].reporter) && Users(Reports[id].reporter).connected) {
@@ -95,26 +92,26 @@ exports.commands = {
 				}
 				this.sendReply("You've denied the report by "+Reports[id].reporter);
 				pmUpperStaff(user.name + " denied the report by " + Reports[id].reporter + ". (ID: " + id + ")");
-				Rooms('upperstaff').add(user.name + " denied the report by " + Reports[id].reporter + ". (ID: " + id + ")");
-				Rooms('upperstaff').update();
+				Rooms('upperstaff').add(user.name + " denied the report by " + Reports[id].reporter + ". (ID: " + id + ")").update();
 				delete Reports[id];
 				saveReports();
 				break;
 			case 'del':
 			case 'delete':
 				if (params.length < 1) return this.errorReply("Usage: /reports delete [id]");
-				var id = params.shift();
+				let id = params.shift();
 				if (!Reports[id]) return this.errorReply("There's no report with that id.");
 				pmUpperStaff(user.name + " deleted the report by " + Reports[id].reporter + ". (ID: " + id + ")");
-				Rooms('upperstaff').add(user.name + " deleted the report by " + Reports[id].reporter + ". (ID: " + id + ")");
-				Rooms('upperstaff').update();
+				Rooms('upperstaff').add(user.name + " deleted the report by " + Reports[id].reporter + ". (ID: " + id + ")").update();
 				delete Reports[id];
 				saveReports();
 				this.sendReply("That report has been deleted.");
 				break;
+			case '':
 			case 'help':
 				if (!this.runBroadcast()) return;
-				this.sendReplyBox("Report commands: <br />" +
+				this.sendReplyBox(
+					"Report commands: <br />" +
 					"/report [message] - Adds a report to the system<br />" +
 					"/reports view - Views all current reports<br />" +
 					"/reports accept [id] - Accepts a report<br />" +

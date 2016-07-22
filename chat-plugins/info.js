deae519315882a915b255388a8ade4e2799a79d4
userNTX <anakin.10@hotmail.com>
2016-07-21 21:38:46 -0400

    Add bot rank

--- chat-plugins/info.js
+++ chat-plugins/info.js
@@ -1668,13 +1668,8 @@ exports.commands = {
 		target = this.canHTML(target);
 		if (!target) return;
 
-		if (user.userid === 'github') {
-			if (!this.can('announce', null, room)) return;
-			if (message.charAt(0) === '!') this.broadcasting = true;
-		} else {
-			if (!this.can('declare', null, room)) return;
-			if (!this.runBroadcast('!htmlbox')) return;
-		}
+		if (!this.can('declare', null, room)) return;
+		if (!this.runBroadcast('!htmlbox')) return;
 
 		this.sendReplyBox(target);
 	},

dad9165e49fc3c7b0043bab5b2528751b511d6ff
zellman01 <zellman01@gmail.com>
2016-07-31 12:37:07 -0400

    ...

--- commands.js
+++ commands.js
@@ -1833,6 +1833,7 @@ exports.commands = {
 
 		this.addModCommand("" + targetUser.name + " was namelocked by " + user.name + "." + (reason ? " (" + reason + ")" : ""));
 		this.globalModlog("NAMELOCK", targetUser, " by " + user.name + (reason ? ": " + reason : ""));
+		console.log("NAMELOCK", targetUser, " by " + user.name + (reason ? ": " + reason : ""));
 		Rooms.global.cancelSearch(targetUser);
 		Punishments.namelock(targetUser, null, null, reason);
 		targetUser.popup("|modal|" + user.name + " has locked your name and you can't change names anymore" + (reason ? ": " + reason : "."));
